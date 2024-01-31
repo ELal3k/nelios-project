dc3e66060793755fa0d508d1c79179deccd32d15
Alexandros Polymeros <alpolymer@gmail.com>
2024-01-31 12:19:22 +0200

    filter button

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import CardGrid from "@/components/CardGrid"
 import SideBar from "@/components/SideBar"
 import Dropdown from "@/components/Dropdown"
+import { AdjustmentsVerticalIcon } from "@heroicons/react/24/outline"
 export default function Home() {
   return (
     <main className="flex mt-16 items-start gap-6 self-stretch">
@@ -9,7 +10,10 @@ export default function Home() {
         <article className="flex lg:flex-row flex-col justify-between items-center lg:px-0 px-20">
           <h2 className="">139 διαθέσιμα πακέτα διακοπών</h2>
           <div className="flex lg:w-auto w-full justify-between">
-            <button className="btn-secondary lg:hidden flex">btn</button>
+            <button className="btn-secondary lg:hidden gap-2 border-accent_green text-accent_green">
+              <AdjustmentsVerticalIcon className="h-5" />
+              <p>Φίλτρα</p>
+            </button>
             <div className="relative">
               <Dropdown />
             </div>
