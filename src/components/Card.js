ceacd29669e7309dab21b2e04cb62118c797a6d5
Alexandros Polymeros <alpolymer@gmail.com>
2024-02-02 11:34:51 +0200

    final

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,12 @@
 import Image from "next/image"
 
 export default function Card({ cardData }) {
-  const { imageSrc, txtSmall, title, price } = cardData
+  const { name, rating, meal_plan, city, price, photo } = cardData
   return (
     <div className="flex flex-col rounded-lg bg-white shadow-xl h-full">
-      <Image
-        src={imageSrc}
+      <img src={photo} className=" rounded-md h-3/5" />
+      {/* <Image
+        src={photo}
         style={{
           width: "100%",
           height: "auto",
@@ -14,16 +15,22 @@ export default function Card({ cardData }) {
         width={500}
         height={500}
         alt="Card Picture"
-      />
+      /> */}
 
       <div className="flex flex-col gap-2 px-6 pb-6 pt-5 h-full justify-between">
         <div>
           <p className="text-small_text_12 font-lightplus text-my_gray">
-            {txtSmall}
+            rating:{rating}
           </p>
-          <h2 className="text-h6 font-semibold text-my_black uppercase">
-            {title}
+          <h2 className="text-h6 font-semibold text-my_black uppercase flex flex-col">
+            <div>{name}</div>
+            <div>{city}</div>
           </h2>
+        </div>
+
+        <div className=" text-my_black flex gap-1 ">
+          <p>Meal Plan:</p>
+          <p>{meal_plan}</p>
         </div>
 
         <div className="flex flex-wrap justify-between items-center">
@@ -31,7 +38,7 @@ export default function Card({ cardData }) {
             <span className="text-small_text_12 text-my_black font-semibold">
               ΑΠΟ
             </span>{" "}
-            <span className="text-h6 text-my_black font-semibold">
+            <span className="text-h7 text-my_black font-semibold">
               {price} €
             </span>
           </p>
--- src/components/CardGrid.js
+++ src/components/CardGrid.js
@@ -1,30 +1,30 @@
 "use client"
 import Card from "@/components/Card"
 import { useAppContext } from "@/context"
-export default function CardGrid({ cardData }) {
+export default function CardGrid({ dataObj }) {
   const { sortPriceOption, minPrice, maxPrice } = useAppContext()
 
-  const sortAndFilterCards = () => {
-    let sortedCards = [...cardData]
+  // const sortAndFilterCards = () => {
+  //   let sortedCards = [...cardData]
 
-    if (sortPriceOption === "ascending") {
-      sortedCards = sortedCards.sort((a, b) => a.price - b.price)
-    } else if (sortPriceOption === "descending") {
-      sortedCards = sortedCards.sort((a, b) => b.price - a.price)
-    }
+  //   if (sortPriceOption === "ascending") {
+  //     sortedCards = sortedCards.sort((a, b) => a.price - b.price)
+  //   } else if (sortPriceOption === "descending") {
+  //     sortedCards = sortedCards.sort((a, b) => b.price - a.price)
+  //   }
 
-    return sortedCards.filter(
-      (card) => card.price >= minPrice && card.price <= maxPrice
-    )
-  }
+  //   return sortedCards.filter(
+  //     (card) => card.price >= minPrice && card.price <= maxPrice
+  //   )
+  // }
 
-  const sortedAndFilteredCards = sortAndFilterCards()
+  // const sortedAndFilteredCards = sortAndFilterCards()
 
   return (
     <ul className="grid lg:grid-cols-3 lg:mx-0 mx-20 grid-cols-1 gap-8">
-      {sortedAndFilteredCards.map((card) => (
-        <li key={card.id}>
-          <Card cardData={card} />
+      {dataObj.data.map((item) => (
+        <li key={item.name}>
+          <Card cardData={item} />
         </li>
       ))}
     </ul>
